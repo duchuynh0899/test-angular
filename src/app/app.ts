ab797d39d588ac5db6f76eeaba23de085eb1a26f
huynh.nd <huynh.nd@aseansc.com.vn>
2026-07-03 10:01:07 +0700

    adđ

---
+++ src/app/app.ts
@@ -0,0 +1,25 @@
+import { Component, signal } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: "app-root",
+  imports: [RouterOutlet],
+  templateUrl: "./app.html",
+  styleUrl: "./app.scss",
+})
+export class App {
+  protected readonly title = signal("testttt");
+
+  constructor() {
+    window.addEventListener("message", (event) => {
+      console.log("testttttttttttttttttttttttttt1111111111111111111");
+      // if (event.origin !== "https://wordpress-domain.com") {
+      //   return;
+      // }
+
+      // if (event.data.type === "language") {
+      //   console.log("testttttttttttttttttttttttttt");
+      // }
+    });
+  }
+}
